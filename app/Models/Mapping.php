7e5c238b05603f837171014ded885521a2cb227a
Neder Alfonso Fandino Andrade <neder.fandino@salesdecameron.com>
2024-04-11 06:46:49 -0500

    feat: Handle missing values in JSON data
    This commit introduces a new feature to handle missing values in JSON data. 
    
    - A new class `JsonWithMissing` has been added under the `App\Casts` namespace. This class implements the `CastsAttributes` interface from Laravel's Eloquent ORM and provides two methods:
      - The `get()` method checks if the value is 'undefined' and returns an instance of `MissingValue`, otherwise it decodes the JSON.
      - The `set()` method checks if the value is an instance of `MissingValue` and returns 'undefined', otherwise it encodes the value into JSON.
    
    - In addition, changes have been made to the `Mapping` model under the `App\Models` namespace:
      - Two properties, 'source_data' and 'mapped_data', are now cast using our newly created custom cast class (`JsonWithMissing`).

--- app/Models/Mapping.php
+++ app/Models/Mapping.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use App\Casts\JsonWithMissing;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Resources\MissingValue;
 
 class Mapping extends Model
 {
     use HasFactory;
+    
+    protected $casts = [
+        'source_data' => JsonWithMissing::class,
+        'mapped_data' => JsonWithMissing::class,
+    ];
+    
 }
