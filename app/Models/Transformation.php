c79237d149a411c9322eec434616ff0584fcf8b7
Neder Alfonso Fandino Andrade <neder.fandino@salesdecameron.com>
2024-04-11 06:46:34 -0500

    feat: Enhance Transformation model with mapping relationships
    In this commit, we have extended the functionality of the `Transformation` model by introducing two new methods - `mapping()` and `mappings()`. 
    
    The `mapping()` method returns an instance of Attribute that represents the first mapping associated with a transformation. The `mappings()` method establishes a many-to-many relationship between the Transformation and Mapping models using the pivot model MappingTransformation. This allows us to easily retrieve all mappings related to a specific transformation.
    
    These changes will facilitate more efficient data retrieval and manipulation in scenarios where transformations are linked with multiple mappings.

--- app/Models/Transformation.php
+++ app/Models/Transformation.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Transformation extends Model
 {
     use HasFactory;
+    
+    public function mapping(): Attribute
+    {
+        return Attribute::make(get: fn() => $this->mappings()->first());
+    }
+    
+    public function mappings(): BelongsToMany
+    {
+        return $this->belongsToMany(Mapping::class)->using(MappingTransformation::class);
+    }
 }
