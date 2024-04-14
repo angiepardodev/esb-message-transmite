1637d540176e4b9a1508c80f0ecc9bc5351b94c4
Neder Alfonso Fandino Andrade <neder.fandino@salesdecameron.com>
2024-04-14 18:30:04 -0500

    feat: Add new API endpoint for message storage
    This commit introduces a new API endpoint to the web routes. The purpose of this change is to enhance our application's functionality by allowing messages to be stored based on specific origin, destination, and service parameters.
    
    Technical Details:
    - Imported MessageController in web.php.
    - Added a POST route 'api/v1/origin/{origin}/destination/{destination}/{service:slug}' which uses the 'store' method from MessageController.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post(
+    'api/v1/origin/{origin}/destination/{destination}/{service:slug}', [MessageController::class, 'store']
+);
